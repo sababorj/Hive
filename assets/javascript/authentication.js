3a0bb37211f7dee1dc63159ab643a72f82060d57
Saba Schock <sababorjsaba@gmail.com>
2018-11-28 11:15:17 -0800

    start with registration logic

---
+++ assets/javascript/authentication.js
@@ -0,0 +1,28 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAchBPc26tTCgCKlzrk-xY4kAdMTS3ot5w",
+    authDomain: "cbc-first.firebaseapp.com",
+    databaseURL: "https://cbc-first.firebaseio.com",
+    projectId: "cbc-first",
+    storageBucket: "cbc-first.appspot.com",
+    messagingSenderId: "462974477285"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+  var auth = firebase.auth();
+//   auth.createUserWithEmailAndPassword("sababorjsaba@gmail.com","123334")
+
+// registering new use
+$(document).on("click","#register", function() {
+
+
+
+
+//  auth.createUserWithEmailAndPassword()
+})
+// sign a user in 
+
+
+// sign out 
+
